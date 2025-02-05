53a0a737ac649e43c25e242ab532525d1678a3ba
Abinand P <abinand0911@gmail.com>
2025-02-05 08:55:00 +0000

    feat: modified the server to parse the TF file
    Signed-off-by: Abinand P <abinand0911@gmail.com>

--- server.js
+++ server.js
@@ -2,65 +2,28 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const { exec } = require("child_process");
-
+const bodyParser = require("body-parser");
 const app = express();
-app.use(express.json());
+app.use(bodyParser.text({ type: "application/x-hcl" }));
 
 const TF_FILE = path.join(__dirname, "temp/config.tf");
+
 const generateTfFile = (data, apiKey) => {
-  // Start the Terraform configuration
-  let tfContent = `
-terraform {
-  required_providers {
-    permitio = {
-      source = "registry.terraform.io/permitio/permit-io"
-    }
-  }
-}
+  const tfContent = data.replace("{{API_KEY}}", `"` + apiKey + `"`);
 
-provider "permitio" {
-  api_key = "${apiKey}"
-}
-
-`;
-
-  // Loop through resources and generate their block in the Terraform file
-  data.resources.forEach((resource) => {
-    tfContent += `
-resource "${resource.type}" "${resource.key}" {
-  key = "${resource.key}"
-  name = "${resource.name}"
-  
-  // Handle actions if present
-  ${resource.actions ? `actions = {
-    ${Object.entries(resource.actions).map(([key, value]) => `
-      "${key}" = {
-        "name" = "${value.name}"
-      }
-    `).join("\n")}
-  }` : ''}
-  
-  // Default empty attributes block
-  attributes = {}
-}
-    `;
-  });
-
-  // Write the generated content to the temp/config.tf file
-  fs.writeFileSync(TF_FILE, tfContent, "utf-8");
+  fs.writeFileSync(TF_FILE, tfContent);
 };
+
 app.post("/apply", (req, res) => {
   try {
-    const apiKey = req.headers['authorization'];
+    const apiKey = req.headers["authorization"];
     if (!apiKey) {
-      return res
-        .status(400)
-        .json({ error: "ApiKey is required to apply the terraform file" });
+      return res.status(400).json({ error: "API Key is required" });
     }
     generateTfFile(req.body, apiKey);
     exec(
       "terraform init && terraform apply -auto-approve",
-      { cwd: __dirname+"/temp"},
+      { cwd: __dirname + "/temp" },
       (error, stdout, stderr) => {
         if (error) {
           return res.status(500).json({ error: stderr || error.message });
@@ -75,5 +38,5 @@ app.post("/apply", (req, res) => {
 
 const PORT = 3456;
 app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
